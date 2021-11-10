d2d771bf81cf2a0be876382eaaafa5fec42ec057
irfan <Irfan00roshan@gmail.com>
2021-11-10 16:48:12 +0530

    Added Tests case for invSBox and added round key add

--- src/test/com/ok/kalyna/KalynaRoundFunctionTest.java
+++ src/test/com/ok/kalyna/KalynaRoundFunctionTest.java
@@ -10,6 +10,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class KalynaRoundFunctionTest {
 
+    private String[] SBoxTestCaseInput = {
+            "050102030405060708090A0B0C0D0E0F",
+            "62C87E6D6EBA4734E5583BDD28CACF8D",
+            "53E95E9206C5D09BBF6097DCA8989844",
+    };
+    private String[] SBoxTestCaseOutput = {
+            "75BB9A4D6BCB452A713ADFB31790511F",
+            "FC4F5E9CC3232E40D98141FC1FD382BF",
+            "5A04E6B66C846B1D26E724A5C50B28E5"
+
+    };
+
+
     @BeforeEach
     void setUp() {
     }
@@ -22,35 +35,40 @@ class KalynaRoundFunctionTest {
     void sBox(){
 
 
-        String[] input = {
-                "050102030405060708090A0B0C0D0E0F",
-                "62C87E6D6EBA4734E5583BDD28CACF8D",
-                "53E95E9206C5D09BBF6097DCA8989844",
+        String[] input = SBoxTestCaseInput;
+        String[] expectedOutput = SBoxTestCaseOutput;
 
-
-        };
-        String[] expectedOutput = {
-                "75BB9A4D6BCB452A713ADFB31790511F",
-                "FC4F5E9CC3232E40D98141FC1FD382BF",
-                "5A04E6B66C846B1D26E724A5C50B28E5"
-
-        };
         assertEquals(input.length,expectedOutput.length);
         for (int i = 0; i < input.length; i++) {
             byte[][] inputState = KalynaUtil.stringToState(input[i]);
             byte[][] val = KalynaUtil.stringToState(expectedOutput[i]);
 
             byte[][] out = KalynaRoundFunction.sBox(inputState);
-
             boolean pass = true;
             for (int j = 0; j < val.length; j++)
                 pass = pass && Arrays.equals(val[j],out[j]);
+
             assertTrue(pass);
         }
     }
 
     @Test
     void invSBox() {
+        String[] input = SBoxTestCaseOutput;
+        String[] expectedOutput =  SBoxTestCaseInput;
+
+        assertEquals(input.length,expectedOutput.length);
+        for (int i = 0; i < input.length; i++) {
+            byte[][] inputState = KalynaUtil.stringToState(input[i]);
+            byte[][] val = KalynaUtil.stringToState(expectedOutput[i]);
+
+            byte[][] out = KalynaRoundFunction.invSBox(inputState);
+            boolean pass = true;
+            for (int j = 0; j < val.length; j++)
+                pass = pass && Arrays.equals(val[j],out[j]);
+
+            assertTrue(pass);
+        }
     }
 
     @Test
